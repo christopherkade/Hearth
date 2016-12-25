4965b152bb4bdbf42b18417c627cc0fedc3cad49
Christopher Kade <christopher.kade@epitech.eu>
2016-12-25 21:39:55 +0100

    Added graphview library

--- app/src/main/java/com/kade_c/hearth/fragments/statistics_fragments/GeneralStatisticsGraph.java
+++ app/src/main/java/com/kade_c/hearth/fragments/statistics_fragments/GeneralStatisticsGraph.java
@@ -7,19 +7,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 import com.kade_c.hearth.R;
+import com.kade_c.hearth.Statistics;
 
 /**
  * Should display various graphics of user deck / game progressions.
  */
+// TODO: Work on applying stats to graph.
 public class GeneralStatisticsGraph extends Fragment {
 
     View view;
+
+    private Statistics statistics;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         view =  inflater.inflate(R.layout.statistics_general_graph, container, false);
+
+        statistics = new Statistics(getContext(), getActivity());
+
+        GraphView graph = (GraphView) view.findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
 
         return view;
     }
@@ -27,6 +46,6 @@ public class GeneralStatisticsGraph extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        getActivity().setTitle("Graph");
+        getActivity().setTitle("Statistics");
     }
 }
